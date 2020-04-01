ccefb39c2765cfaf6e60557dd8d7dec7e0b1b96d
twagner <thomas.wagner@live.fr>
2020-04-01 18:35:04 +0200

    Task list v0.1

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { AddForm } from './AddForm'
 import { SideBar } from './SideBar'
 import { TaskList } from './TaskList'
+import { v4 as uuidv4 } from 'uuid';
 import "./TaskContainer.css"
 
 export const TaskContainer = () => {
@@ -10,8 +11,26 @@ export const TaskContainer = () => {
 
     const addTask = (e) => {
         e.preventDefault()
-        setTasks([...tasks,document.getElementById('input_task').value])
+        setTasks([...tasks,
+            {
+                id:uuidv4(),
+                status:'opened',
+                label:document.getElementById('input_task').value
+            }
+        ])
         document.getElementById('input_task').value = "";
+    }
+
+    const checkTask = (e) => {
+        setTasks(tasks.map(task => 
+            (task.id === e.target.id)?{id:task.id,status:'closed',label:task.label}:task
+        ))   
+    }
+
+    const uncheckTask = (e) => {
+        setTasks(tasks.map(task => 
+            (task.id === e.target.id)?{id:task.id,status:'opened',label:task.label}:task
+        ))   
     }
 
     return (
@@ -19,7 +38,7 @@ export const TaskContainer = () => {
             <AddForm addTask={addTask} />
             <div id="content_main">
                 <SideBar />
-                <TaskList taskList={tasks} />
+                <TaskList taskList={tasks} checkTask={checkTask} uncheckTask={uncheckTask}/>
             </div>
         </div>
     )
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import "./TaskList.css"
+import { FaRegSquare, FaCheckSquare } from 'react-icons/fa'
 
 export const TaskList = (props) => {
 
     return (
         <div id="task_list">            
            <section className="openedTasks">
+                <h2>Opened tasks</h2>
+                {
+                    props.taskList.filter(task => task.status === 'opened').length === 0
+                    && <p>You have no opened tasks.</p>
+                }
                 <ul>
-                    {
-                        props.taskList.map((task) => {
-                        return <li><span>box </span><span>{task}</span></li>
-                        })
-                    }
+                {
+                    props.taskList.map((task,index) => {
+                        if (task.status === 'opened'){                     
+                            return (
+                                <li key={index}>
+                                    <span className="checkbox"><FaRegSquare id={task.id} onClick={props.checkTask}/></span>
+                                    <span> {task.label}</span>
+                                </li>
+                            )
+                        } else {
+                            return ""
+                        }
+                    })
+                }
+                </ul>
+            </section>
+            <section className="closedTasks">
+                <h2>Closed tasks</h2>
+                {
+                    props.taskList.filter(task => task.status === 'closed').length === 0
+                    && <p>You have no closed tasks.</p>
+                }
+                <ul>
+                {
+                    props.taskList.map((task,index) => {
+                        if (task.status === 'closed'){                     
+                            return (
+                                <li key={index}>
+                                    <span className="checkbox"><FaCheckSquare id={task.id} onClick={props.uncheckTask}/></span>
+                                    <span> {task.label}</span>
+                                </li>
+                            )
+                        } else {
+                            return ""
+                        }
+                    })
+                }
                 </ul>
             </section>
         </div>
